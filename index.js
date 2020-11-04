37ff54f06fc662d837f4958ce6e9bb5d9dd8ad08
ccastillos <ccastillo@soldai.com>
2020-11-03 19:05:35 -0600

    dependencies installed

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+require('dotenv').config();
+const ZabbixSender = require('node-zabbix-sender');
+
+const Sender = new ZabbixSender({
+  host: process.env.ZABBIX_SERVER,
+  items_host: process.env.ZABBIX_HOSTNAME,
+});
+
+const oldConsole = console;
+const newConsole = Object.keys(oldConsole).reduce((newConsole, key) => {
+  newConsole[key] = (...input) => {
+    oldConsole[key](...input);
+    Sender.addItem('console.' + key, JSON.stringify(input)).send();
+  };
+  return newConsole;
+}, {});
+
+console = newConsole;
+
+module.exports = { Sender };
